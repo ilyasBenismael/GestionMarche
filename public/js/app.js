a8d7bcbe62aacf0bf7cc0762c16e412d6d4a96b8
Im-MB <bmarzouq03@gmail.com>
2023-05-29 12:50:52 +0100

    MB V3.2.0

--- public/js/app.js
+++ public/js/app.js
@@ -1,10 +1,69 @@
 
 
 
-
+/*
 
 document.querySelector('.tumbler-wrapper').addEventListener('click', _ => document.body.classList.toggle('night-mode'));
 document.querySelector('.tumbler-wrapper').addEventListener('click', _ => document.getElementById("dataTable").classList.toggle('night-mode'));
+
+*/
+const tumblerWrapper = document.querySelector('.tumbler-wrapper');
+const checkbox = document.querySelector('.switch');
+const dataTable = document.getElementById('dataTable');
+
+// Check if the mode is stored in localStorage
+const storedMode = localStorage.getItem('mode');
+
+// Set the initial mode based on the stored value or default to 'light-mode'
+const initialMode = storedMode || 'light-mode';
+document.body.classList.add(initialMode);
+
+// Function to toggle the mode
+function toggleMode() {
+    // Toggle the mode class on the body
+    document.body.classList.toggle('night-mode');
+
+    // Toggle the mode class on the dataTable element
+    if (document.body.classList.contains('night-mode')) {
+        dataTable.classList.remove('night-mode');
+    } else {
+        dataTable.classList.add('night-mode');
+    }
+
+    // Store the current mode in localStorage
+    const currentMode = document.body.classList.contains('night-mode') ? 'night-mode' : 'light-mode';
+    localStorage.setItem('mode', currentMode);
+}
+
+// Add event listener to the tumblerWrapper
+tumblerWrapper.addEventListener('click', toggleMode);
+
+// Update the checkbox state based on the initial mode
+checkbox.checked = initialMode === 'night-mode';
+
+// Apply the initial mode class to the dataTable element
+if (initialMode === 'night-mode') {
+    dataTable.classList.remove('night-mode');
+} else {
+    dataTable.classList.add('night-mode');
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
